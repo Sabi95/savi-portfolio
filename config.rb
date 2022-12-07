63758043c0fc740045b96d4217e5550023c25984
Savithri Wewala <s.w.wewala@gmail.com>
2022-12-07 14:46:45 +0900

    added meta tags

--- config.rb
+++ config.rb
@@ -13,6 +13,9 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+
+activate :metaman, host: 'https://savithriwewala.me'
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
